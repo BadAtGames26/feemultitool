8bde2b349f73f635e5bedae483297ea880f04e30
BadAtGames26 <125292563+BadAtGames26@users.noreply.github.com>
2025-03-26 11:30:51 -0700

    Improvements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(path_file_prefix)]
 
 use clap::{Args, Parser, Subcommand};
+use dialoguer::Select;
 use image::{DynamicImage, GenericImage, ImageReader, Rgba};
 use imageproc::map::*;
 use core::panic;
@@ -9,7 +10,7 @@ use std::path::Path;
 #[derive(Parser)]
 struct Cli {
     #[command(subcommand)]
-    command: Commands,
+    command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
@@ -43,20 +44,104 @@ struct NormalArgs {
 fn main() {
     let args = Cli::parse();
     match args.command {
-        Commands::SplitMulti(args) => {
-            split(args);
+        Some(Commands::SplitMulti(args)) => {
+            split(args.multi_path);
         }
-        Commands::JoinMulti(args) => {
-            join(args);
+        Some(Commands::JoinMulti(args)) => {
+            join(args.r_path, args.g_path, args.b_path, args.a_path);
         },
-        Commands::FixNormal(args) => {
-            normal(args);
+        Some(Commands::FixNormal(args)) => {
+            normal(args.normal_path);
         },
+        None => {
+            let items = ["1 - Fix Normal", "2 - Split Multi", "3 - Join Multi"];
+            let command = Select::new()
+            .with_prompt("Choose a command (Use arrows and enter to confirm)")
+            .items(&items)
+            .default(0)
+            .interact()
+            .unwrap();
+            match command {
+                0 => {
+                    let normal_path = native_dialog::FileDialog::new()
+                    .set_title("Select Normal Map")
+                    .add_filter("PNG Image", &["png"])
+                    .show_open_single_file()
+                    .unwrap()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_string();
+
+                    normal(normal_path);                        
+                }
+
+                1 => {
+                    let multi_path = native_dialog::FileDialog::new()
+                    .set_title("Select Multi Map")
+                    .add_filter("PNG Image", &["png"])
+                    .show_open_single_file()
+                    .unwrap()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_string();
+
+                    split(multi_path);    
+                }
+
+                2 => {
+                    let paths = native_dialog::FileDialog::new()
+                    .set_title("Select Multi Map Channel Images")
+                    .add_filter("PNG Image", &["png"])
+                    .show_open_multiple_file()
+                    .unwrap();
+
+                    if paths.len() != 4 {
+                        panic!("Four images should be selected");
+                    } else {
+                        let r_path = paths.iter()
+                        .find(|f| f.to_str().unwrap().ends_with("_R.png"))
+                        .expect("Should have a Red Channel Image.")                        
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+
+                        let g_path = paths.iter()
+                        .find(|f| f.to_str().unwrap().ends_with("_G.png"))
+                        .expect("Should have a Green Channel Image.")                        
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+
+                        let b_path = paths.iter()
+                        .find(|f| f.to_str().unwrap().ends_with("_B.png"))
+                        .expect("Should have a Blue Channel Image.")                        
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+                    
+                        let a_path = paths.iter()
+                        .find(|f| f.to_str().unwrap().ends_with("_A.png"))
+                        .expect("Should have a Alpha Channel Image.")
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+
+                        join(r_path, g_path, b_path, a_path);
+                    }
+                }
+
+                _ => {
+                    panic!("Invalid Command");
+                }
+            }
+        }
     }
 }
 
-fn split(args: SplitArgs) {
-    let path = Path::new(&args.multi_path);
+fn split(multi_path: String) {
+    let path = Path::new(&multi_path);
     let image = ImageReader::open(path).expect("Multi Path should be an image.");
     let binding = image.decode().unwrap().to_rgba8();
     for x in 0..4 {
@@ -75,11 +160,11 @@ fn split(args: SplitArgs) {
     }
 }
 
-fn join(args: JoinArgs) {
-    let r_image = ImageReader::open(Path::new(&args.r_path)).expect("R Path should be an image.").decode().unwrap().to_rgba8();
-    let g_image = ImageReader::open(Path::new(&args.g_path)).expect("G Path should be an image.").decode().unwrap().to_rgba8();
-    let b_image = ImageReader::open(Path::new(&args.b_path)).expect("B Path should be an image.").decode().unwrap().to_rgba8();
-    let a_image = ImageReader::open(Path::new(&args.a_path)).expect("A Path should be an image.").decode().unwrap().to_rgba8();
+fn join(r_path: String, g_path: String, b_path: String, a_path: String,) {
+    let r_image = ImageReader::open(Path::new(&r_path)).expect("R Path should be an image.").decode().unwrap().to_rgba8();
+    let g_image = ImageReader::open(Path::new(&g_path)).expect("G Path should be an image.").decode().unwrap().to_rgba8();
+    let b_image = ImageReader::open(Path::new(&b_path)).expect("B Path should be an image.").decode().unwrap().to_rgba8();
+    let a_image = ImageReader::open(Path::new(&a_path)).expect("A Path should be an image.").decode().unwrap().to_rgba8();
 
     if r_image.width() != ( g_image.width() | b_image.width() | a_image.width() ) {
         panic!("Multi channels do not match one or more widths.");
@@ -93,17 +178,17 @@ fn join(args: JoinArgs) {
             let r_pixel = r_image.get_pixel(w, h)[0];
             let g_pixel = g_image.get_pixel(w, h)[1];
             let b_pixel = b_image.get_pixel(w, h)[2];
-            let a_pixel = a_image.get_pixel(w, h)[3];
+            let a_pixel = a_image.get_pixel(w, h)[1];
             let pixel = Rgba::from([r_pixel, g_pixel, b_pixel, a_pixel]);
             multi.put_pixel(w, h, pixel);
         }
     }
-    let save_path = Path::new(&args.r_path).file_prefix().unwrap().to_str().unwrap().to_string() + "GBA.png";
+    let save_path = Path::new(&r_path).file_prefix().unwrap().to_str().unwrap().to_string() + "GBA.png";
     multi.save_with_format(save_path, image::ImageFormat::Png).unwrap();
 }
 
-fn normal(args: NormalArgs) {
-    let path = Path::new(&args.normal_path);
+fn normal(normal_path: String) {
+    let path = Path::new(&normal_path);
     let image = ImageReader::open(path).expect("Normal Path should be an image.");
     let binding = image.decode().unwrap().to_rgba8();
     let rgb = map_colors(&binding, |p| { Rgba([p[3], p[1], p[0], p[0]]) });
